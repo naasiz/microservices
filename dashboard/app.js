8f8c37c6602d0e0c26ad82bbbd7f5868105fa89e
naasiz <nastaranzirak511@gmail.com>
2025-04-10 15:20:37 -0700

    endpoints

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,8 +1,12 @@
 const analyzerStatsEl = document.getElementById('analyzerStats');
 const processingStatsEl = document.getElementById('processingStats');
+const quizEventEl = document.getElementById('quizEvent');
+const questionEventEl = document.getElementById('questionEvent');
 
 const analyzerUrl = 'http://20.163.57.7:8110/stats';
 const processingUrl = 'http://20.163.57.7:8100/stats';
+const quizEventUrl = 'http://20.163.57.7:8110/quiz-event?index=0';
+const questionEventUrl = 'http://20.163.57.7:8110/question-event?index=0';
 
 function fetchStats(url, element) {
   fetch(url)
@@ -22,8 +26,12 @@ function fetchStats(url, element) {
 setInterval(() => {
   fetchStats(analyzerUrl, analyzerStatsEl);
   fetchStats(processingUrl, processingStatsEl);
+  fetchStats(quizEventUrl, quizEventEl);
+  fetchStats(questionEventUrl, questionEventEl);
 }, 5000);
 
 // Initial fetch
 fetchStats(analyzerUrl, analyzerStatsEl);
 fetchStats(processingUrl, processingStatsEl);
+fetchStats(quizEventUrl, quizEventEl);
+fetchStats(questionEventUrl, questionEventEl);
